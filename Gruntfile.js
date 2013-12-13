f9a44873edd52ce42181e05b77b5099f44ba2568
jefe <yukatan@gmail.com>
2013-12-13 20:01:18 +0100

    update remote address for push

--- Gruntfile.js
+++ Gruntfile.js
@@ -294,7 +294,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'https://github.com/yukatan/commangular',
           branch: 'gh-pages',
           commit: true,
           push: true
