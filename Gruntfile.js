76e06e987a6953746abadb1149041d86d708db69
jefe <yukatan@gmail.com>
2013-12-14 01:17:08 +0100

    add bower components

--- Gruntfile.js
+++ Gruntfile.js
@@ -259,6 +259,7 @@ module.exports = function (grunt) {
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             '_bower_components/jquery/jquery.js',
+            '_bower_components/font-awesome/fonts',
             'favicon.ico',
             'apple-touch*.png'
           ],
