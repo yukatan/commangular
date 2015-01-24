9a9884051dce4dc317169ad98a865b44bb442c88
yukatan <yukatan@gmail.com>
2015-01-24 19:39:43 +0100

    c

---
+++ js/d16b.script.js
@@ -0,0 +1,1 @@
+!function(){for(var a,b=function(){},c=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeStamp","trace","warn"],d=c.length,e=window.console=window.console||{};d--;)a=c[d],e[a]||(e[a]=b)}();
